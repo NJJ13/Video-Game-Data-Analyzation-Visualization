9394ed1f4245c87d3f170afc0524676acf22bebe
Nick Johnson <Johnsn0913@gmail.com>
2021-03-17 10:37:59 -0500

    updated the search function

--- datatracker/game.py
+++ datatracker/game.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 
 import requests, json
 from types import SimpleNamespace
@@ -13,4 +13,34 @@ def display_games():
     response = requests.get('https://api.dccresource.com/api/games')
     games = json.loads(response.content, object_hook=lambda d: SimpleNamespace(**d))
     return render_template('games/index.html', games=games)
+
+
+@bp.route('/games/search', methods=('GET', 'POST'))
+def search_games():
+    if request.method == 'POST':
+        id = request.form['_id']
+        error = None
+
+        if not id:
+            error = 'You must enter an ID'
+
+        if error is not None:
+            flash(error)
+        elif request.form['_id'] != "":
+            return redirect(url_for('games.game_details', id=id))
+        else:
+            return render_template('games/search.html', id=id)
+
+    else:
+        return render_template('games/search.html', id="")
+
+
+@bp.route('/games/<id>', methods=['GET'])
+def game_details(id):
+    response = requests.get('https://api.dccresource.com/api/games')
+    games = json.loads(response.content, object_hook=lambda d: SimpleNamespace(**d))
+    for game in games:
+        if game._id == id:
+            found_game = game
+    return render_template('games/details.html', found_game=found_game)
 
